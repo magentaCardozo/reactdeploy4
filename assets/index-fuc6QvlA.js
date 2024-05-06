033f64ce5934f17a7b290a044f12639aa78d15da
Patrick Kalambayi Mulu <patrickmulu2005@gmail.com>
2024-05-06 13:08:51 +0100

    Updates

--- assets/index-fuc6QvlA.js
+++ assets/index-fuc6QvlA.js
@@ -1665,13 +1665,9 @@ position : relative;
       position:relative;
       box-shadow: 7px 0 5px -5px grey, -7px 0 5px -5px grey;
     height:100%;
-    width: 90%;
-    flex-shrink:0;
+    width: 90% !important;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items:center;
-    
     border-radius:5px;
     padding:5px;
   }
